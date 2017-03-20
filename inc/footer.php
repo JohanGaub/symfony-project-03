3027bf4f657a2f87122446a7e833a4f3b1c604e9
Johan Gaub <johangaub@gmail.com>
2017-03-20 17:22:44 +0100

    new strusture from Julien

--- inc/footer.php
+++ inc/footer.php
@@ -1,7 +1,47 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: topikana
- * Date: 20/03/17
- * Time: 15:17
- */
+
+
+<div class="container-fluid">
+        <div class="panel panel-default">
+            <div id="footerpart1" class="panel-body">
+                <div id="myicon" class="row list-inline">
+                    <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 BLOCK01"></div>
+                    <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 BLOCK02">
+                        <ul>
+                            <li><a href="https://fr-fr.facebook.com/" target="_blank"><i
+                                    class="fa fa-facebook-square" aria-hidden="true"></i></a></li>
+                            <li><a href="https://twitter.com" target="_blank">
+                                <span class="fa-stack">
+                                    <i class="fa fa-circle fa-stack-2x"></i>
+                                    <i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
+                                </span>
+                            </a></li>
+
+                            <li><a href="https://fr.linkedin.com/" target="_blank"><i class="fa fa-linkedin-square"
+                                                                                      aria-hidden="true"></i></a>
+                            </li>
+                            <li><a href="https://www.www.inside_cummunit.fr" target="_blank"><i class="fa fa-users"
+                                                                                                aria-hidden="true"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 BLOCK03"></div>
+                </div>
+            </div>
+
+            <div id="footerpart2" class="panel-footer">
+                <div class="row">
+                    <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 BLOCK01"></div>
+                    <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 BLOCK02">
+                        <p>&copy; commun<span>it</span>.fr
+<p>
+                    </div>
+                    <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 BLOCK03"></div>
+                </div>
+            </div>
+
+
+        </div>
+
+    </div>
+    </div>
