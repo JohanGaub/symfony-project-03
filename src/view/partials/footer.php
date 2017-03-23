0fc36213be8022b53477c2bc98aa04608c8d367c
François LETELLIER <f.letellier0@gmail.com>
2017-03-23 11:51:52 +0100

    finish part 1 not all responsive, put triangle more

--- src/view/partials/footer.php
+++ src/view/partials/footer.php
@@ -1,1 +1,7 @@
- <p> vcn,ddkvdndkv</p>
+
+<div class="panel panel-default">
+    <div class="panel-body foot1">
+        Panel body ...
+    </div>
+
+</div>
---
+++ src/view/partials/header.php
@@ -0,0 +1,14 @@
+<div class="navbar class = top">
+    <a class="navbar-brand" href="#">Title</a>
+    <ul class="nav navbar-nav">
+        <li class="active">
+            <a href="#">Home</a>
+        </li>
+        <li>
+            <a href="#">Link</a>
+        </li>
+    </ul>
+</div>
+
+
+
