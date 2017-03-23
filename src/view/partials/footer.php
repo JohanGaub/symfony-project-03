cf93a305088ce9ee246c0a790ef950de7fbf1be2
Johan Gaub <johangaub@gmail.com>
2017-03-23 13:42:16 +0100

    footer renamed for php include, head filled with meta

--- src/view/partials/footer.php
+++ src/view/partials/footer.php
@@ -1,20 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="../../../public/css/footer.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
-    <link rel="stylesheet" href="https://opensource.keycdn.com/fontawesome/4.7.0/font-awesome.min.css" integrity="sha384-dNpIIXE8U05kAbPhy3G1cz+yZmTzA6CY8Vg/u2L9xRnHjJiAK76m2BIEaSEV+/aU" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome-animation/0.0.10/font-awesome-animation.css" integrity="sha256-nBFRd4iXQgAv308dzcrSrU5vkKAT4SMXdZLJfN7MZeg=" crossorigin="anonymous">
-
-    <title>Footer</title>
-</head>
-<body>
-
-<footer>
     <div class="container-fluid">
         <div class="panel panel-default">
 
@@ -50,8 +33,8 @@
                                     <i class="fa fa-users fa-stack-1x fa-inverse"></i>
                                 </span>
                             </a></li>
-
                         </ul>
+
                     </div>
                 </div>
 
@@ -64,7 +47,6 @@
                         </ul>
                     </div>
                 </div>
-
             </div>
 
             <div id="footerpart2" class="panel-footer">
@@ -76,7 +58,7 @@
                         <ul class="centerUL">
                             <li><a href="#">Contactez-nous</a></li>
                             <li><a href="tel:+33671058591">06 71 05 85 91</a></li>
-                            <li><a href="tel:+33428382467">06 71 05 85 91</a></li>
+                            <li><a href="tel:+33428382467">04 28 38 24 67</a></li>
                         </ul>
                     </div>
                     <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
@@ -87,9 +69,5 @@
                     </div>
                 </div>
             </div>
-
         </div>
     </div>
-</footer>
-</body>
-</html>
