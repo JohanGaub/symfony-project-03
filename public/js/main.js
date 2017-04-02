1cf41e2d93ae79e3a061b76b3c1098b8c85278b5
Irena Jakubec-Berenguel <irena.jakubec@gmail.com>
2017-04-02 17:30:57 +0200

    Make navbar scrollable for large screens

--- public/js/main.js
+++ public/js/main.js
@@ -5,3 +5,18 @@ $("#slider1").responsiveSlides({
     prevText: "<span class='glyphicon glyphicon-chevron-left'></span>",
     nextText: "<span class='glyphicon glyphicon-chevron-right'></span>"
 });
+
+$(document).ready(function () {
+
+
+    // animate navbar on scroll
+    $(window).scroll(function () {
+        if (screen.width >= 1024) {
+            if ($(document).scrollTop() > 100) {
+                $('.navbar, .navbar-default .navbar-brand, .navbar-brand img, .navbar-collapse').addClass('scroll');
+            } else {
+                $('.navbar, .navbar-default .navbar-brand, .navbar-brand img, .navbar-collapse').removeClass('scroll');
+            }
+        }
+    });
+});
