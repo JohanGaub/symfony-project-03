5395c845209b32a2890ee2d455979f8e6e0e79d9
Johan Gaub <johangaub@gmail.com>
2017-04-04 17:40:50 +0200

    3 first blocks-beginning

--- src/view/partials/communit.php
+++ src/view/partials/communit.php
@@ -1,1 +1,19 @@
-<?php
+<div class="flexcontainer">
+
+        <div class="flex">
+            <p>La COMMUN’it est un groupement de revendeurs au niveau national mais avant tout un réseau de partenaires convaincus, qui souhaitent se libérer des contraintes imposées par les constructeurs traditionnels et des formations et certifications obligatoires et coûteuses.</p>
+        </div>
+
+        <div class="flex">
+            <p>La COMMUN’it vous offre la possibilité d’interagir avec nos développeurs afin de faire évoluer la solution itnéo en répondant aux attentes de vos clients et de vos services techniques. Vous pourrez via l’extranet proposer vos compétences techniques au réseau d’intégrateurs et en rechercher afin d’être toujours plus efficace sur le terrain.</p>
+        </div>
+
+        <div class="flex">
+            <p>Pas de formations obligatoires, nous vous accompagnons sur le terrain aussi bien commercialement que techniquement. Des formations spécifiques peuvent être proposées ou demandées.</p>
+        </div>
+
+</div>
+
+
+
+
