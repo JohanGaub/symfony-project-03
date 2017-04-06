3da47943b98df8657636708edae46709a4289af9
JulienMa <mastromonaco.julien@gmail.com>
2017-04-06 17:16:11 +0200

    Fix a responsive, need to fix mediaqueries again

--- src/view/partials/indexItneo.php
+++ src/view/partials/indexItneo.php
@@ -1,44 +1,44 @@
-<div id="index-itneo" class="row BORDER01">
+<div id="index-itneo" class="row">
     <!-- Part top (logo + h3 text -> descript product) -->
     <div class="col-xs-12 itneo-top-case">
         <img class="it-logo be-inline" src="../img/itneo_logo_HD.png" alt="">
         <h2 class="it-h2 be-inline">Notre solution téléphonie ip<br>basée sur Astérisk</h2>
     </div>
-</div>
 
-<!-- Part down (3parts -> text-info -> slider -> button -->
-<div class="row BORDER02">
-    <!-- First col (text-info) -->
-    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-3 BORDER03">
-        <ul class="it-first-list">
-            <li>Solution logicielle</li>
-            <li>Innovante</li>
-            <li>Basée sur Asterisk</li>
-            <li>Sans licence / utilisateur</li>
-            <li>En intelligence partagé</li>
-            <li>Marque blanche</li>
-            <li>Marge optimisée</li>
-        </ul>
-    </div>
-
-    <!-- Second col (slider) -->
-    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-6 BORDER04">
-        <div class="it-neo-slider">
-            <!-- General slider div -->
-            <ul class="rslides" id="slider1">
-                <li><img src="img/1.jpg" alt=""></li>
-                <li><img src="img/2.jpg" alt=""></li>
-                <li><img src="img/3.jpg" alt=""></li>
+    <!-- Part down (3parts -> text-info -> slider -> button -->
+    <div class="row">
+        <!-- First col (text-info) -->
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-3 it-ul-box-1">
+            <ul class="it-first-list">
+                <li>Solution logicielle</li>
+                <li>Innovante</li>
+                <li>Basée sur Asterisk</li>
+                <li>Sans licence / utilisateur</li>
+                <li>En intelligence partagé</li>
+                <li>Marque blanche</li>
+                <li>Marge optimisée</li>
             </ul>
         </div>
-        <a href="#" class="it-link-1">Plus d'info</a>
-    </div>
 
-    <!-- Last part (button) -->
-    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-3 BORDER05">
-        <div class="it-btn-group">
-            <button type="button" class="btn-itneo">Découvrez Itnéo</button>
-            <button type="button" class="btn-joinus">Rejoingez-nous</button>
+        <!-- Second col (slider) -->
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-6 out-slider-block">
+            <div class="it-neo-slider">
+                <!-- General slider div -->
+                <ul class="rslides" id="slider1">
+                    <li><img src="img/1.jpg" alt=""></li>
+                    <li><img src="img/2.jpg" alt=""></li>
+                    <li><img src="img/3.jpg" alt=""></li>
+                </ul>
+            </div>
+            <a href="#" class="it-link-1">Plus d'info</a>
+        </div>
+
+        <!-- Last part (button) -->
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-3">
+            <div class="it-btn-group">
+                <button type="button" class="btn-itneo">Découvrez Itnéo</button>
+                <button type="button" class="btn-joinus">Rejoingez-nous</button>
+            </div>
         </div>
     </div>
 </div>
