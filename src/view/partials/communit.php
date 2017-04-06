dc160d7a9ce345cd8704ab8bae30eaa939c5c1c1
Johan Gaub <johangaub@gmail.com>
2017-04-06 15:54:43 +0200

    3 buttons chosen and set

--- src/view/partials/communit.php
+++ src/view/partials/communit.php
@@ -1,24 +1,30 @@
-<div class="flexcontainer">
 
 
-    <div class="dessous">
-        <div class="flex">
-            <p>La COMMUN’it est un groupement de revendeurs au niveau national mais avant tout un réseau de partenaires convaincus, qui souhaitent se libérer des contraintes imposées par les constructeurs traditionnels et des formations et certifications obligatoires et coûteuses.</p>
-        </div>
-    </div>
-
-    <div class="dessous">
-        <div class="flex">
-            <p>La COMMUN’it vous offre la possibilité d’interagir avec nos développeurs afin de faire évoluer la solution itnéo en répondant aux attentes de vos clients et de vos services techniques. Vous pourrez via l’extranet proposer vos compétences techniques au réseau d’intégrateurs et en rechercher afin d’être toujours plus efficace sur le terrain.</p>
-        </div>
-    </div>
-
-    <div class="dessous">
-        <div class="flex">
-            <p>Pas de formations obligatoires, nous vous accompagnons sur le terrain aussi bien commercialement que techniquement. Des formations spécifiques peuvent être proposées ou demandées.</p>
-        </div>
-    </div>
+<div class="general_font">
+    <h1 class ="titleH1">Votre communauté de la téléphonie IP</h1>
 </div>
+
+<section class="flexcontainer">
+
+
+    <div class="frame">
+        <aside class="block_font">
+            <p>La COMMUN’it est un groupement de revendeurs au niveau national mais avant tout un réseau de partenaires convaincus, qui souhaitent se libérer des contraintes imposées par les constructeurs traditionnels et des formations et certifications obligatoires et coûteuses.</p>
+        </aside>
+    </div>
+
+    <div class="frame">
+        <aside class="block_font">
+            <p>La COMMUN’it vous offre la possibilité d’interagir avec nos développeurs afin de faire évoluer la solution itnéo en répondant aux attentes de vos clients et de vos services techniques. Vous pourrez via l’extranet proposer vos compétences techniques au réseau d’intégrateurs et en rechercher afin d’être toujours plus efficace sur le terrain.</p>
+        </aside>
+    </div>
+
+    <div class="frame">
+        <aside class="block_font">
+            <p>Pas de formations obligatoires, nous vous accompagnons sur le terrain aussi bien commercialement que techniquement. Des formations spécifiques peuvent être proposées ou demandées.</p>
+        </aside>
+    </div>
+</section>
 
 
 
