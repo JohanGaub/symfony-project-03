7f4116c8339718df42d309f6e314cce27ac1f923
JulienMa <mastromonaco.julien@gmail.com>
2017-04-20 17:06:36 +0200

    Add small fix

--- web/assets/js/main.js
+++ web/assets/js/main.js
@@ -1,13 +1,10 @@
 // Swiper Slider
 $(document).ready(function () {
     var swiper = new Swiper('.swiper-container', {
-        pagination: '.swiper-pagination',
         nextButton: '.swiper-button-next',
         prevButton: '.swiper-button-prev',
-        paginationClickable: true,
         spaceBetween: 20,
-        centeredSlides: true,
-        autoplay: 60000,
+        autoplay: 6000,
         loop: true,
         autoplayDisableOnInteraction: false
     });
@@ -19,21 +16,21 @@ $(document).ready(function () {
     // animate navbar on scroll
     $(window).scroll(function () {
 
-            if ($(document).scrollTop() > 100) {
-                $('.navbar, .navbar-default .navbar-brand, .navbar-brand img, .navbar-collapse').addClass('scroll');
-            } else {
-                $('.navbar, .navbar-default .navbar-brand, .navbar-brand img, .navbar-collapse').removeClass('scroll');
-            }
+        if ($(document).scrollTop() > 100) {
+            $('.navbar, .navbar-default .navbar-brand, .navbar-brand img, .navbar-collapse').addClass('scroll');
+        } else {
+            $('.navbar, .navbar-default .navbar-brand, .navbar-brand img, .navbar-collapse').removeClass('scroll');
+        }
 
     });
 
     $(window).scroll(function () {
 
-            if ($(document).scrollTop() > 80) {
-                $('.navbar, .navbar-default .navbar-brand, .navbar-brand img, .navbar-collapse').addClass('scroll-midd');
-            } else {
-                $('.navbar, .navbar-default .navbar-brand, .navbar-brand img, .navbar-collapse').removeClass('scroll-midd');
-            }
+        if ($(document).scrollTop() > 80) {
+            $('.navbar, .navbar-default .navbar-brand, .navbar-brand img, .navbar-collapse').addClass('scroll-midd');
+        } else {
+            $('.navbar, .navbar-default .navbar-brand, .navbar-brand img, .navbar-collapse').removeClass('scroll-midd');
+        }
 
     });
 
