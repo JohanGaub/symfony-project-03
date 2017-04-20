a023d078c9e596c0cadffd72348398af05696f68
JulienMa <mastromonaco.julien@gmail.com>
2017-04-20 21:37:33 +0200

    Add 'mentions legales' modal + link

--- web/assets/js/main.js
+++ web/assets/js/main.js
@@ -1,3 +1,16 @@
+// Modal
+$(document).ready(function () {
+    $(".modal-fullscreen").on('show.bs.modal', function () {
+        setTimeout( function() {
+            $(".modal-backdrop").addClass("modal-backdrop-fullscreen");
+        }, 0);
+    });
+    $(".modal-fullscreen").on('hidden.bs.modal', function () {
+        $(".modal-backdrop").addClass("modal-backdrop-fullscreen");
+    });
+});
+
+
 // Swiper Slider
 $(document).ready(function () {
     var swiper = new Swiper('.swiper-container', {
