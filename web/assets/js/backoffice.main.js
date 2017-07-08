813aca1bf0f09f40f498803621b9e4c8c48f8433
JuMastro <mastromonaco.julien@gmail.com>
2017-07-08 18:09:36 +0200

    [Fix] - Fix front - Space from header - Remove js sroll part in sidebar control js parts

--- web/assets/js/backoffice.main.js
+++ web/assets/js/backoffice.main.js
@@ -1,28 +1,7 @@
 /**
  * Sidebar control parts
  */
-function htmlbodyHeightUpdate(){
-    let height1 = $('.nav').height()+50
-    let height2 = $('.main').height()
-    let height3 = $( window ).height()
-
-    if (height2 > height3) {
-        $('html').height(Math.max(height1,height3,height2)+10);
-        $('body').height(Math.max(height1,height3,height2)+10);
-    } else {
-        $('html').height(Math.max(height1,height3,height2));
-        $('body').height(Math.max(height1,height3,height2));
-    }
-}
 $(document).ready(function () {
-    htmlbodyHeightUpdate()
-    $(window).resize(function() {
-        htmlbodyHeightUpdate()
-    });
-    $(window).scroll(function() {
-        htmlbodyHeightUpdate()
-    });
-
     let focus       = '.sidebar'
     let target      = 'header .dropdown'
     let cssClass    = 'open'
